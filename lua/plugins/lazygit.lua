7ba74a65997538133b766eaaaf181da72a6e74e7
jazzmagus <diego.fantinelli@gmail.com>
2025-04-16 18:12:35 +0200

    aggiornamento con Lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,8 +1,14 @@
 return {
   "kdheepak/lazygit.nvim",
   lazy = false,
-  enabled = false,
+  -- enabled = false,  <-- Rimuovi o imposta a true
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
   config = function()
     vim.g.lazygit_floating_window_scaling_factor = 1.0
+    -- Aggiungi queste variabili d'ambiente
+    vim.env.GIT_SSH_COMMAND = "ssh -i ~/.ssh/id_rsa" -- Percorso alla tua chiave privata
+    vim.env.SSH_AUTH_SOCK = vim.fn.expand("$SSH_AUTH_SOCK")
   end,
 }
